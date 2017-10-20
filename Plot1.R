da7fe461520ebeb8e8b8ec54c4fcee396923421f
Lim Pong Hui <ponghui85@gmail.com>
2017-10-20 23:24:32 +0800

    added data loading steps

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,18 @@
+#loading data
+library(lubridate)
+library(dplyr)
+data <- read.csv2('../data/household_power_consumption.txt',sep=";",
+                  colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),
+                  dec=".",
+                  na.strings="?")
+
+data2 <- data %>% 
+        mutate( Datetime=as.POSIXct(paste(Date,Time, sep=" "),format="%d/%m/%Y %H:%M:%S")) %>%
+        filter(Datetime >= ymd("2007/02/01") & Datetime < ymd("2007/02/03"))
+
+
+
+
 
 #assign png device
 png(filename="Plot1.png",width = 480, height = 480, units = "px", pointsize = 12)
